675a4fdfa7a2825ed9bb59cc505ecaec1a48cf2c
Aayush <aayushg2908@gmail.com>
2025-03-02 20:32:07 +0530

    fix: google models update

--- actions/index.ts
+++ actions/index.ts
@@ -44,7 +44,7 @@ export const generateResponse = async (index: number, prompt: string) => {
   ];
 
   const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY!);
-  const model = genAI.getGenerativeModel({ model: "gemini-pro" });
+  const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
   const gen_prompt = `${prompts[index]}
   ${prompt}
